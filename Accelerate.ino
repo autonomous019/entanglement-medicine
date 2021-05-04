062f136e3a36c6de3782904613851c5b5bd02040
mike mccarron <m.j.h.mccarron@gmail.com>
2021-05-03 23:00:51 -0700

    Add files via upload

---
+++ Accelerate.ino
@@ -0,0 +1,96 @@
+int buzzerPin = 9;
+
+
+void setup() {
+  
+  pinMode(9, OUTPUT);
+  
+  
+}
+
+void loop() {
+  //DON'T FORGET TO CHANGE THE VALUES BACK TO ORIGINAL
+
+  
+  delay(random(1,20));
+  
+  digitalWrite(9,HIGH);
+  delay(3);
+  
+
+  digitalWrite(9,LOW);
+  delay(24);
+  digitalWrite(9,HIGH);
+  delay(3);
+  digitalWrite(9,LOW);
+  delay(22);
+  digitalWrite(9,HIGH);
+  delay(3);
+  digitalWrite(9,LOW);
+  delay(20);
+  digitalWrite(9,HIGH);
+  delay(3);
+  
+
+  digitalWrite(9,LOW);
+  delay(random(1,20));
+  digitalWrite(9,HIGH);
+  delay(3);
+
+
+  digitalWrite(9,LOW);
+  delay(18);
+  digitalWrite(9,HIGH);
+  delay(3);
+  digitalWrite(9,LOW);
+  delay(16);
+  digitalWrite(9,HIGH);
+  delay(3);
+  digitalWrite(9,LOW);
+  delay(14);
+  digitalWrite(9,HIGH);
+  delay(3);
+  
+  digitalWrite(9,LOW);
+  delay(random(1,20));
+  digitalWrite(9,HIGH);
+  delay(3);
+
+
+
+  digitalWrite(9,LOW);
+  delay(12);
+  digitalWrite(9,HIGH);
+  delay(3);
+  digitalWrite(9,LOW);
+  delay(10);
+  digitalWrite(9,HIGH);
+  delay(3);
+  digitalWrite(9,LOW);
+  delay(8);
+  digitalWrite(9,HIGH);
+  delay(3);
+  
+  digitalWrite(9,LOW);
+  delay(random(1,20));
+  digitalWrite(9,HIGH);
+  delay(3);
+
+
+  digitalWrite(9,LOW);
+  delay(6);
+  digitalWrite(9,HIGH);
+  delay(3);
+  digitalWrite(9,LOW);
+  delay(4);
+  digitalWrite(9,HIGH);
+  delay(3);
+  digitalWrite(9,LOW);
+  delay(2);
+  digitalWrite(9,HIGH);
+  delay(3);
+  
+  
+
+  
+}
